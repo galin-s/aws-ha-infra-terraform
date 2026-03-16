3da42e611f499e8ed8f1c23e63966328884c927a
Galin Stefanov <galin.stef@abv.bg>
2026-03-16 23:31:13 +0200

    feat: Add Security module

---
+++ modules/security/outputs.tf
@@ -0,0 +1,19 @@
+output "alb_sg_id" {
+  description = "ALB Security Group ID"
+  value       = aws_security_group.alb_sg.id
+}
+
+output "ec2_sg_id" {
+  description = "EC2 Security Group ID"
+  value       = aws_security_group.ec2_sg.id
+}
+
+output "ec2_sg_name" {
+  description = "EC2 Security Group Name"
+  value       = aws_security_group.ec2_sg.name
+}
+
+output "rds_sg_id" {
+  description = "RDS Security Group ID"
+  value = aws_security_group.rds_sg.id
+}
